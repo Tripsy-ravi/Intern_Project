60e33267538cd6ba5d8b6649019ba177cc8d3a78
Ravi Tripathi <tripathiravi207@gmail.com>
2021-12-15 13:57:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/android/internproject/ChefNavigation.java
@@ -0,0 +1,14 @@
+package com.example.android.internproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChefNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_navigation);
+    }
+}
